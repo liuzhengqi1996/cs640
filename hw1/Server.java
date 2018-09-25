6f045786ca9c85cc9a89dddf43d085f78e294c95
Kendall Park <darkidder@gmail.com>
2018-09-25 03:06:55 -0500

    add @plipka7's server

--- hw1/Server.java
+++ hw1/Server.java
@@ -1,11 +1,32 @@
+import java.net.*;
+import java.io.*;
+
 public class Server implements Runnable {
   int port;
+  char[] buffer;
 
   public Server(int port) {
     this.port = port;
+    buffer = new char[1000];
   }
 
   public void run() {
-    
+    try(
+    	ServerSocket ss = new ServerSocket(port);
+    	Socket cs = ss.accept();
+    	BufferedReader in = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+    		) {
+    	long start = System.currentTimeMillis();
+    	long runTime;
+    	int kbTransfered = 0;
+    	while(in.read(buffer, 0, 1000) != -1) {
+    		kbTransfered++;
+    	}
+    	runTime = System.currentTimeMillis() - start;
+
+    	System.out.printf("received=%d KB rate=%3.3f Mbps\n", kbTransfered, ((double)kbTransfered)/runTime);
+    	// System.out.printf("rate=%3.3f Mbps\n", );
+    } catch(IOException e) {
+    }
   }
 }
