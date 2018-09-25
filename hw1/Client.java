d900ea7d590bd033fd28d22e354d2fe3d2a19f0e
Kendall Park <darkidder@gmail.com>
2018-09-24 19:46:55 -0500

    add functionality to client

--- hw1/Client.java
+++ hw1/Client.java
@@ -1,15 +1,38 @@
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.io.IOException;
+import java.io.BufferedOutputStream;
+
 public class Client implements ThingThatRuns {
   String hostname;
   int port;
-  int time; // in seconds
+  int milliseconds; // in milliseconds
 
   public Client(String hostname, int port, int time) {
     this.hostname = hostname;
     this.port = port;
-    this.time = time;
+    this.milliseconds = time * 1000;
   }
 
   public void run() {
-
+    try {
+      Socket socket = new Socket(this.hostname, this.port);
+      BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
+      byte message[] = new byte[1000];
+      long startTime = System.currentTimeMillis();
+      int kilobytesSent = 0;
+      while (System.currentTimeMillis() < startTime + this.milliseconds) {
+        System.out.println("Hello");
+        out.write(message);
+        kilobytesSent++;
+      }
+      socket.close();
+      double mbps = ((double) kilobytesSent) / this.milliseconds;
+      System.out.println(String.format("sent=%s KB rate=%s Mbps", kilobytesSent, mbps));
+    } catch(UnknownHostException u) {
+        System.out.println(u);
+    } catch(IOException i) {
+        System.out.println(i);
+    }
   }
 }
