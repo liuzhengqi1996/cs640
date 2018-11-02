d3caf46658df740d7f43bbe936fd0dc60a1e0d24
Kendall Park <darkidder@gmail.com>
2018-11-02 17:29:09 -0500

    add official prof's code

--- hw3/src/edu/wisc/cs/sdn/vnet/rt/RouteTable.java
+++ hw3/src/edu/wisc/cs/sdn/vnet/rt/RouteTable.java
@@ -17,17 +17,17 @@ import edu.wisc.cs.sdn.vnet.Iface;
  * Route table for a router.
  * @author Aaron Gember-Jacobson
  */
-public class RouteTable
+public class RouteTable 
 {
 	/** Entries in the route table */
-	private List<RouteEntry> entries;
-
+	private List<RouteEntry> entries; 
+	
 	/**
 	 * Initialize an empty route table.
 	 */
 	public RouteTable()
 	{ this.entries = new LinkedList<RouteEntry>(); }
-
+	
 	/**
 	 * Lookup the route entry that matches a given IP address.
 	 * @param ip IP address
@@ -35,29 +35,31 @@ public class RouteTable
 	 */
 	public RouteEntry lookup(int ip)
 	{
-		synchronized(this.entries) {
+		synchronized(this.entries)
+        {
 			/*****************************************************************/
 			/* TODO: Find the route entry with the longest prefix match      */
-			System.out.println("Entries");
-			System.out.println(this.entries);
-
-			RouteEntry match = null;
-
-			for (RouteEntry entry : this.entries) {
-				int d1 = entry.getMaskAddress() & ip;
-				if (d1 == entry.getDestinationAddress()) {
-					if (match == null) {
-						match = entry;
-					} else if (entry.getMaskAddress() > match.getMaskAddress()) {
-						match = entry;
-					}
-				}
-			}
-			return match;
+			
+	        RouteEntry bestMatch = null;
+	        for (RouteEntry entry : this.entries)
+	        {
+	           int maskedDst = ip & entry.getMaskAddress();
+	           int entrySubnet = entry.getDestinationAddress() 
+	               & entry.getMaskAddress();
+	           if (maskedDst == entrySubnet)
+	           {
+	        	   if ((null == bestMatch) 
+	        		   || (entry.getMaskAddress() > bestMatch.getMaskAddress()))
+	        	   { bestMatch = entry; }
+	           }
+	        }
+			
+			return bestMatch;
+			
 			/*****************************************************************/
-    }
+        }
 	}
-
+	
 	/**
 	 * Populate the route table from a file.
 	 * @param filename name of the file containing the static route table
@@ -68,39 +70,39 @@ public class RouteTable
 	{
 		// Open the file
 		BufferedReader reader;
-		try
+		try 
 		{
 			FileReader fileReader = new FileReader(filename);
 			reader = new BufferedReader(fileReader);
 		}
-		catch (FileNotFoundException e)
+		catch (FileNotFoundException e) 
 		{
 			System.err.println(e.toString());
 			return false;
 		}
-
+		
 		while (true)
 		{
 			// Read a route entry from the file
 			String line = null;
-			try
+			try 
 			{ line = reader.readLine(); }
-			catch (IOException e)
+			catch (IOException e) 
 			{
 				System.err.println(e.toString());
 				try { reader.close(); } catch (IOException f) {};
 				return false;
 			}
-
+			
 			// Stop if we have reached the end of the file
 			if (null == line)
 			{ break; }
-
+			
 			// Parse fields for route entry
 			String ipPattern = "(\\d+\\.\\d+\\.\\d+\\.\\d+)";
 			String ifacePattern = "([a-zA-Z0-9]+)";
 			Pattern pattern = Pattern.compile(String.format(
-                        "%s\\s+%s\\s+%s\\s+%s",
+                        "%s\\s+%s\\s+%s\\s+%s", 
                         ipPattern, ipPattern, ipPattern, ifacePattern));
 			Matcher matcher = pattern.matcher(line);
 			if (!matcher.matches() || matcher.groupCount() != 4)
@@ -118,9 +120,9 @@ public class RouteTable
 				try { reader.close(); } catch (IOException f) {};
 				return false;
 			}
-
+			
 			int gwIp = IPv4.toIPv4Address(matcher.group(2));
-
+			
 			int maskIp = IPv4.toIPv4Address(matcher.group(3));
 			if (0 == maskIp)
 			{
@@ -129,7 +131,7 @@ public class RouteTable
 				try { reader.close(); } catch (IOException f) {};
 				return false;
 			}
-
+			
 			String ifaceName = matcher.group(4).trim();
 			Iface iface = router.getInterface(ifaceName);
 			if (null == iface)
@@ -139,33 +141,33 @@ public class RouteTable
 				try { reader.close(); } catch (IOException f) {};
 				return false;
 			}
-
+			
 			// Add an entry to the route table
 			this.insert(dstIp, gwIp, maskIp, iface);
 		}
-
+	
 		// Close the file
 		try { reader.close(); } catch (IOException f) {};
 		return true;
 	}
-
+	
 	/**
 	 * Add an entry to the route table.
 	 * @param dstIp destination IP
 	 * @param gwIp gateway IP
 	 * @param maskIp subnet mask
-	 * @param iface router interface out which to send packets to reach the
+	 * @param iface router interface out which to send packets to reach the 
 	 *        destination or gateway
 	 */
 	public void insert(int dstIp, int gwIp, int maskIp, Iface iface)
 	{
 		RouteEntry entry = new RouteEntry(dstIp, gwIp, maskIp, iface);
         synchronized(this.entries)
-        {
+        { 
             this.entries.add(entry);
         }
 	}
-
+	
 	/**
 	 * Remove an entry from the route table.
 	 * @param dstIP destination IP of the entry to remove
@@ -173,7 +175,7 @@ public class RouteTable
      * @return true if a matching entry was found and removed, otherwise false
 	 */
 	public boolean remove(int dstIp, int maskIp)
-	{
+	{ 
         synchronized(this.entries)
         {
             RouteEntry entry = this.find(dstIp, maskIp);
@@ -183,7 +185,7 @@ public class RouteTable
         }
         return true;
     }
-
+	
 	/**
 	 * Update an entry in the route table.
 	 * @param dstIP destination IP of the entry to update
@@ -192,7 +194,7 @@ public class RouteTable
 	 * @param iface new router interface for matching entry
      * @return true if a matching entry was found and updated, otherwise false
 	 */
-	public boolean update(int dstIp, int maskIp, int gwIp,
+	public boolean update(int dstIp, int maskIp, int gwIp, 
             Iface iface)
 	{
         synchronized(this.entries)
@@ -219,20 +221,20 @@ public class RouteTable
             for (RouteEntry entry : this.entries)
             {
                 if ((entry.getDestinationAddress() == dstIp)
-                    && (entry.getMaskAddress() == maskIp))
+                    && (entry.getMaskAddress() == maskIp)) 
                 { return entry; }
             }
         }
         return null;
     }
-
+	
 	public String toString()
 	{
         synchronized(this.entries)
-        {
+        { 
             if (0 == this.entries.size())
             { return " WARNING: route table empty"; }
-
+            
             String result = "Destination\tGateway\t\tMask\t\tIface\n";
             for (RouteEntry entry : entries)
             { result += entry.toString()+"\n"; }
--- hw3/src/edu/wisc/cs/sdn/vnet/rt/Router.java
+++ hw3/src/edu/wisc/cs/sdn/vnet/rt/Router.java
@@ -6,19 +6,18 @@ import edu.wisc.cs.sdn.vnet.Iface;
 
 import net.floodlightcontroller.packet.Ethernet;
 import net.floodlightcontroller.packet.IPv4;
-import net.floodlightcontroller.packet.MACAddress;
 
 /**
  * @author Aaron Gember-Jacobson and Anubhavnidhi Abhashkumar
  */
 public class Router extends Device
-{
+{	
 	/** Routing table for the router */
 	private RouteTable routeTable;
-
+	
 	/** ARP cache for the router */
 	private ArpCache arpCache;
-
+	
 	/**
 	 * Creates a router for a specific host.
 	 * @param host hostname for the router
@@ -29,13 +28,13 @@ public class Router extends Device
 		this.routeTable = new RouteTable();
 		this.arpCache = new ArpCache();
 	}
-
+	
 	/**
 	 * @return routing table for the router
 	 */
 	public RouteTable getRouteTable()
 	{ return this.routeTable; }
-
+	
 	/**
 	 * Load a new routing table from a file.
 	 * @param routeTableFile the name of the file containing the routing table
@@ -48,13 +47,13 @@ public class Router extends Device
 					+ routeTableFile);
 			System.exit(1);
 		}
-
+		
 		System.out.println("Loaded static route table");
 		System.out.println("-------------------------------------------------");
 		System.out.print(this.routeTable.toString());
 		System.out.println("-------------------------------------------------");
 	}
-
+	
 	/**
 	 * Load a new ARP cache from a file.
 	 * @param arpCacheFile the name of the file containing the ARP cache
@@ -67,7 +66,7 @@ public class Router extends Device
 					+ arpCacheFile);
 			System.exit(1);
 		}
-
+		
 		System.out.println("Loaded static ARP cache");
 		System.out.println("----------------------------------");
 		System.out.print(this.arpCache.toString());
@@ -83,75 +82,96 @@ public class Router extends Device
 	{
 		System.out.println("*** -> Received packet: " +
                 etherPacket.toString().replace("\n", "\n\t"));
-
+		
 		/********************************************************************/
 		/* TODO: Handle packets                                             */
-
-		if (etherPacket.getEtherType() != Ethernet.TYPE_IPv4 ) {
-			System.out.println("return");
-			return;
+		
+		switch(etherPacket.getEtherType())
+		{
+		case Ethernet.TYPE_IPv4:
+			this.handleIpPacket(etherPacket, inIface);
+			break;
+		// Ignore all other packet types, for now
 		}
-
-		IPv4 payload = (IPv4) etherPacket.getPayload();
-		short checksum = payload.getChecksum();
-		payload.setChecksum((short)0);
-		payload.serialize();
-
-		if (checksum != payload.getChecksum()) {
-			System.out.println("bad checksum; dropping");
-			return;
-		}
-
-		int ttl = payload.getTtl() - 1;
-		if (ttl < 1) {
-			System.out.println("TTL < 1; dropping");
-			return;
-		}
-		payload.setTtl((byte) ttl);
-		payload.setChecksum((short)0);
-		payload.serialize();
-
-		for (Iface iface : this.interfaces.values()) {
-			if (payload.getDestinationAddress() == iface.getIpAddress()) {
-				System.out.println("Matched Iface; dropping");
-				return;
-			}
-		}
-
-		RouteEntry nextHop = this.routeTable.lookup(payload.getDestinationAddress());
-		if (nextHop == null) {
-			System.out.println("next hop is null; dropping");
-			return;
-		}
-
-		System.out.println(nextHop);
-		System.out.println(this.arpCache);
-
-		ArpEntry destinationARP;
-		if (nextHop.getGatewayAddress() != 0) {
-			System.out.println("Gateway is next.");
-			destinationARP = this.arpCache.lookup(nextHop.getGatewayAddress());
-		} else {
-			destinationARP = this.arpCache.lookup(payload.getDestinationAddress());
-		}
-
-		if (destinationARP == null) {
-			System.out.println("destination ARP is null; dropping");
-			return;
-		}
-		System.out.println("Destination ARP");
-		System.out.println(destinationARP);
-		MACAddress destinationMAC = destinationARP.getMac();
-		Iface outgoingIface = nextHop.getInterface();
-
-		etherPacket.setSourceMACAddress(outgoingIface.getMacAddress().toBytes());
-		etherPacket.setDestinationMACAddress(destinationMAC.toBytes());
-
-		System.out.println("etherPacket");
-		System.out.println(etherPacket);
-
-		this.sendPacket(etherPacket, outgoingIface);
-
+		
 		/********************************************************************/
 	}
+	
+	private void handleIpPacket(Ethernet etherPacket, Iface inIface)
+	{
+		// Make sure it's an IP packet
+		if (etherPacket.getEtherType() != Ethernet.TYPE_IPv4)
+		{ return; }
+		
+		// Get IP header
+		IPv4 ipPacket = (IPv4)etherPacket.getPayload();
+        System.out.println("Handle IP packet");
+
+        // Verify checksum
+        short origCksum = ipPacket.getChecksum();
+        ipPacket.resetChecksum();
+        byte[] serialized = ipPacket.serialize();
+        ipPacket.deserialize(serialized, 0, serialized.length);
+        short calcCksum = ipPacket.getChecksum();
+        if (origCksum != calcCksum)
+        { return; }
+        
+        // Check TTL
+        ipPacket.setTtl((byte)(ipPacket.getTtl()-1));
+        if (0 == ipPacket.getTtl())
+        { return; }
+        
+        // Reset checksum now that TTL is decremented
+        ipPacket.resetChecksum();
+        
+        // Check if packet is destined for one of router's interfaces
+        for (Iface iface : this.interfaces.values())
+        {
+        	if (ipPacket.getDestinationAddress() == iface.getIpAddress())
+        	{ return; }
+        }
+		
+        // Do route lookup and forward
+        this.forwardIpPacket(etherPacket, inIface);
+	}
+
+    private void forwardIpPacket(Ethernet etherPacket, Iface inIface)
+    {
+        // Make sure it's an IP packet
+		if (etherPacket.getEtherType() != Ethernet.TYPE_IPv4)
+		{ return; }
+        System.out.println("Forward IP packet");
+		
+		// Get IP header
+		IPv4 ipPacket = (IPv4)etherPacket.getPayload();
+        int dstAddr = ipPacket.getDestinationAddress();
+
+        // Find matching route table entry 
+        RouteEntry bestMatch = this.routeTable.lookup(dstAddr);
+
+        // If no entry matched, do nothing
+        if (null == bestMatch)
+        { return; }
+
+        // Make sure we don't sent a packet back out the interface it came in
+        Iface outIface = bestMatch.getInterface();
+        if (outIface == inIface)
+        { return; }
+
+        // Set source MAC address in Ethernet header
+        etherPacket.setSourceMACAddress(outIface.getMacAddress().toBytes());
+
+        // If no gateway, then nextHop is IP destination
+        int nextHop = bestMatch.getGatewayAddress();
+        if (0 == nextHop)
+        { nextHop = dstAddr; }
+
+        // Set destination MAC address in Ethernet header
+        ArpEntry arpEntry = this.arpCache.lookup(nextHop);
+        if (null == arpEntry)
+        { return; }
+        etherPacket.setDestinationMACAddress(arpEntry.getMac().toBytes());
+        
+        this.sendPacket(etherPacket, outIface);
+    }
 }
