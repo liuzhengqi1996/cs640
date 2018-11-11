83e311e5c0230b23c3f4211014a067b76d00b445
Kendall Park <darkidder@gmail.com>
2018-11-11 10:47:12 -0600

    add contains method

--- hw3/src/edu/wisc/cs/sdn/vnet/rt/RouteTable.java
+++ hw3/src/edu/wisc/cs/sdn/vnet/rt/RouteTable.java
@@ -17,17 +17,17 @@ import edu.wisc.cs.sdn.vnet.Iface;
  * Route table for a router.
  * @author Aaron Gember-Jacobson
  */
-public class RouteTable 
+public class RouteTable
 {
 	/** Entries in the route table */
-	private List<RouteEntry> entries; 
-	
+	private List<RouteEntry> entries;
+
 	/**
 	 * Initialize an empty route table.
 	 */
 	public RouteTable()
 	{ this.entries = new LinkedList<RouteEntry>(); }
-	
+
 	/**
 	 * Lookup the route entry that matches a given IP address.
 	 * @param ip IP address
@@ -39,27 +39,27 @@ public class RouteTable
         {
 			/*****************************************************************/
 			/* TODO: Find the route entry with the longest prefix match      */
-			
+
 	        RouteEntry bestMatch = null;
 	        for (RouteEntry entry : this.entries)
 	        {
 	           int maskedDst = ip & entry.getMaskAddress();
-	           int entrySubnet = entry.getDestinationAddress() 
+	           int entrySubnet = entry.getDestinationAddress()
 	               & entry.getMaskAddress();
 	           if (maskedDst == entrySubnet)
 	           {
-	        	   if ((null == bestMatch) 
+	        	   if ((null == bestMatch)
 	        		   || (entry.getMaskAddress() > bestMatch.getMaskAddress()))
 	        	   { bestMatch = entry; }
 	           }
 	        }
-			
+
 			return bestMatch;
-			
+
 			/*****************************************************************/
         }
 	}
-	
+
 	/**
 	 * Populate the route table from a file.
 	 * @param filename name of the file containing the static route table
@@ -70,39 +70,39 @@ public class RouteTable
 	{
 		// Open the file
 		BufferedReader reader;
-		try 
+		try
 		{
 			FileReader fileReader = new FileReader(filename);
 			reader = new BufferedReader(fileReader);
 		}
-		catch (FileNotFoundException e) 
+		catch (FileNotFoundException e)
 		{
 			System.err.println(e.toString());
 			return false;
 		}
-		
+
 		while (true)
 		{
 			// Read a route entry from the file
 			String line = null;
-			try 
+			try
 			{ line = reader.readLine(); }
-			catch (IOException e) 
+			catch (IOException e)
 			{
 				System.err.println(e.toString());
 				try { reader.close(); } catch (IOException f) {};
 				return false;
 			}
-			
+
 			// Stop if we have reached the end of the file
 			if (null == line)
 			{ break; }
-			
+
 			// Parse fields for route entry
 			String ipPattern = "(\\d+\\.\\d+\\.\\d+\\.\\d+)";
 			String ifacePattern = "([a-zA-Z0-9]+)";
 			Pattern pattern = Pattern.compile(String.format(
-                        "%s\\s+%s\\s+%s\\s+%s", 
+                        "%s\\s+%s\\s+%s\\s+%s",
                         ipPattern, ipPattern, ipPattern, ifacePattern));
 			Matcher matcher = pattern.matcher(line);
 			if (!matcher.matches() || matcher.groupCount() != 4)
@@ -120,9 +120,9 @@ public class RouteTable
 				try { reader.close(); } catch (IOException f) {};
 				return false;
 			}
-			
+
 			int gwIp = IPv4.toIPv4Address(matcher.group(2));
-			
+
 			int maskIp = IPv4.toIPv4Address(matcher.group(3));
 			if (0 == maskIp)
 			{
@@ -131,7 +131,7 @@ public class RouteTable
 				try { reader.close(); } catch (IOException f) {};
 				return false;
 			}
-			
+
 			String ifaceName = matcher.group(4).trim();
 			Iface iface = router.getInterface(ifaceName);
 			if (null == iface)
@@ -141,33 +141,33 @@ public class RouteTable
 				try { reader.close(); } catch (IOException f) {};
 				return false;
 			}
-			
+
 			// Add an entry to the route table
 			this.insert(dstIp, gwIp, maskIp, iface);
 		}
-	
+
 		// Close the file
 		try { reader.close(); } catch (IOException f) {};
 		return true;
 	}
-	
+
 	/**
 	 * Add an entry to the route table.
 	 * @param dstIp destination IP
 	 * @param gwIp gateway IP
 	 * @param maskIp subnet mask
-	 * @param iface router interface out which to send packets to reach the 
+	 * @param iface router interface out which to send packets to reach the
 	 *        destination or gateway
 	 */
 	public void insert(int dstIp, int gwIp, int maskIp, Iface iface)
 	{
 		RouteEntry entry = new RouteEntry(dstIp, gwIp, maskIp, iface);
         synchronized(this.entries)
-        { 
+        {
             this.entries.add(entry);
         }
 	}
-	
+
 	/**
 	 * Remove an entry from the route table.
 	 * @param dstIP destination IP of the entry to remove
@@ -175,7 +175,7 @@ public class RouteTable
      * @return true if a matching entry was found and removed, otherwise false
 	 */
 	public boolean remove(int dstIp, int maskIp)
-	{ 
+	{
         synchronized(this.entries)
         {
             RouteEntry entry = this.find(dstIp, maskIp);
@@ -185,7 +185,7 @@ public class RouteTable
         }
         return true;
     }
-	
+
 	/**
 	 * Update an entry in the route table.
 	 * @param dstIP destination IP of the entry to update
@@ -194,7 +194,7 @@ public class RouteTable
 	 * @param iface new router interface for matching entry
      * @return true if a matching entry was found and updated, otherwise false
 	 */
-	public boolean update(int dstIp, int maskIp, int gwIp, 
+	public boolean update(int dstIp, int maskIp, int gwIp,
             Iface iface)
 	{
         synchronized(this.entries)
@@ -221,20 +221,24 @@ public class RouteTable
             for (RouteEntry entry : this.entries)
             {
                 if ((entry.getDestinationAddress() == dstIp)
-                    && (entry.getMaskAddress() == maskIp)) 
+                    && (entry.getMaskAddress() == maskIp))
                 { return entry; }
             }
         }
         return null;
     }
-	
+
+	public boolean contains(int dstIp, int maskIp) {
+		return this.find(dstIp, maskIp) == null ? false : true;
+	}
+
 	public String toString()
 	{
         synchronized(this.entries)
-        { 
+        {
             if (0 == this.entries.size())
             { return " WARNING: route table empty"; }
-            
+
             String result = "Destination\tGateway\t\tMask\t\tIface\n";
             for (RouteEntry entry : entries)
             { result += entry.toString()+"\n"; }
