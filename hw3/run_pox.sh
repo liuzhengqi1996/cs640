358f831363e05dde66e97f95bd9ceccfbecd2812
Kendall Park <darkidder@gmail.com>
2018-11-11 10:45:13 -0600

    add more switches and routes to pox config

--- hw3/run_pox.sh
+++ hw3/run_pox.sh
@@ -3,8 +3,12 @@ ovs-vsctl set bridge s1 protocols=OpenFlow10
 ovs-vsctl set bridge s2 protocols=OpenFlow10
 ovs-vsctl set bridge s3 protocols=OpenFlow10
 ovs-vsctl set bridge s4 protocols=OpenFlow10
+ovs-vsctl set bridge s5 protocols=OpenFlow10
+ovs-vsctl set bridge s6 protocols=OpenFlow10
 ovs-vsctl set bridge r1 protocols=OpenFlow10
 ovs-vsctl set bridge r2 protocols=OpenFlow10
 ovs-vsctl set bridge r3 protocols=OpenFlow10
 ovs-vsctl set bridge r4 protocols=OpenFlow10
+ovs-vsctl set bridge r5 protocols=OpenFlow10
+ovs-vsctl set bridge r6 protocols=OpenFlow10
 python ./pox/pox.py cs640.ofhandler cs640.vnethandler
